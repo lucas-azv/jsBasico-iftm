a5b8bc85e2764dff53ec3fa8538a4aaa0e155dba
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-09-18 23:30:29 -0300

    Delete lista5/lista5 directory

--- lista5/lista5/ex01/ex01.js
+++
@@ -1,9 +0,0 @@
-function pegarTexto(event) {
-    event.preventDefault();
-    var texto = document.getElementById("txtNome").value.trim();
-    if (texto === "") {
-        alert("Insira um texto");
-    } else {
-        alert(texto);
-    }
-}
--- lista5/lista5/ex03/ex03.js
+++
@@ -1,7 +0,0 @@
-function calcular(){
-    numero = document.getElementById("n").value;
-    
-    dobro = parseFloat(numero)*2;
-
-    document.getElementById("dobro").value = dobro;
-}
