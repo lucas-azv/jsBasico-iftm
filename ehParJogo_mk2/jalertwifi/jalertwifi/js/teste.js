1e07ff77fa649c2c175d5a42b2fedd142fbb02e6
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-11-13 21:07:39 -0300

    Delete ehParJogo_mk2 directory

--- ehParJogo_mk2/jalertwifi/jalertwifi/js/teste.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-    document.getElementById("btnTestar").addEventListener("click", exibirMsg);
-
-    function exibirMsg() {
-        alertWifi(`Olá, tudo bem?`, false, 0, "img/logo.png", 30, "");
-    }
-});
