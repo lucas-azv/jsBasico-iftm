8e9457d0b50482c64136709439682b89bbc9b5a2
Lucas <108193174+lucas-azv@users.noreply.github.com>
2023-11-26 12:16:40 -0300

    Add files via upload

--- ehParJogo/cadastro/cadastro.js
+++ ehParJogo/cadastro/cadastro.js
@@ -1,51 +1,33 @@
 window.addEventListener("load", function() {
-    document.getElementById("btnCadastrar").addEventListener("click", cadastrarUsuario);
+    this.document.getElementById("btnCadastrar").addEventListener("click", cadastrarUsuario);
 
     function cadastrarUsuario() {
-        var user = document.getElementById("inputUser").value;
-        var pwd = document.getElementById("inputPwd").value;
-        var checkPwd = document.getElementById("inputCheckPwd").value;
+        user = document.getElementById("inputUser").value;
+        pwd = document.getElementById("inputPwd").value;
+        checkPwd = document.getElementById("inputCheckPwd").value;
 
-        if (user == "" || pwd == "" || checkPwd == "") {
-            alertWifi("Preencha todos os campos!", false, 0, "", 30, "");
+        if(user == "" || pwd == "" || checkPwd == "") {
+            alertWifi("Preencha todos os campos!",false,0,"",30,"");
 
-        } else if (pwd != checkPwd) {
-            alertWifi("As senhas não coincidem!", false, 0, "", 30, "");
-
-        } else if (user.split(' ').length > 1) {
-            alertWifi("Nome de usuário inválido", false, 0, "", 30, "");
-
-        } else {
+        } else if(pwd != checkPwd) {
+            alertWifi("As senhas não coincidem!",false,0,"",30,"");
+        }
+        else{
+            var newUser = {nome: user, senha: pwd};
             var vetUsers = localStorage.getItem("vetUsers");
 
-            if (!vetUsers) {
+            if(!vetUsers) {
                 var vet = [];
-                vet.push({ nome: user, senha: pwd });
-                localStorage.setItem("vetUsers", JSON.stringify(vet));
-                alertWifi("Usuário cadastrado com sucesso!", true, 0, "", 30, "");
-                setTimeout(function() {
-                    window.location.href = "../login/index.html";
-                }, 3000);
-
-            } else {
+                vet.push(newUser);
+                localStorage.setItem("vetUsers",JSON.stringify(vet));
+                alertWifi("Usuário cadastrado com sucesso!",true,0,"",30,"");
+            } else{
                 vet = JSON.parse(vetUsers);
-
-                var usuarioExistente = vet.some(function(existingUser) {
-                    return existingUser.nome === user;
-                });
-
-                if (usuarioExistente) {
-                    alertWifi("Usuário já cadastrado.", false, 0, "", 30, "");
-
-                } else {
-                    vet.push({ nome: user, senha: pwd });
-                    localStorage.setItem("vetUsers", JSON.stringify(vet));
-                    alertWifi("Usuário cadastrado com sucesso!", false, 0, "", 30, "");
-                    setTimeout(function() {
-                        window.location.href = "../login/index.html";
-                    }, 3000);
-                }
+                vet.push(newUser);
+                localStorage.setItem("vetUsers",JSON.stringify(vet));
             }
+            alertWifi("Usuário cadastrado com sucesso!",false,0,"",30,"");
         }
     }
+
 });
