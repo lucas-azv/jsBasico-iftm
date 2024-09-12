96acfb1779a1ef23a6aa26e2266a6a09e6e4b030
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-09-12 16:19:35 -0300

    Resolvendo warning do SonarLint (javascript:S905)
    Corrigido o código removendo a expressão sem efeito na função addVotosCandidato, agora o valor de vetorVotos é incrementado corretamente antes de atualizar a interface.

--- 25.09.23/js/scripts.js
+++ 25.09.23/js/scripts.js
@@ -3,19 +3,18 @@ document.getElementById("votosCand2").innerHTML = 0;
 document.getElementById("votosCand3").innerHTML = 0;
 document.getElementById("votosCand4").innerHTML = 0;
 
-vetorVotos = [0,0,0,0];
-candidato1 = document.getElementById("candidato1")
-candidato2 = document.getElementById("candidato2")
-candidato3= document.getElementById("candidato3")
-candidato4 = document.getElementById("candidato4")
+let vetorVotos = [0, 0, 0, 0];
+let candidato1 = document.getElementById("candidato1");
+let candidato2 = document.getElementById("candidato2");
+let candidato3 = document.getElementById("candidato3");
+let candidato4 = document.getElementById("candidato4");
 
-candidato1.addEventListener("click", function() {addVotosCandidato(1)})
-candidato2.addEventListener("click", function() {addVotosCandidato(2)})
-candidato3.addEventListener("click", function() {addVotosCandidato(3)})
-candidato4.addEventListener("click", function() {addVotosCandidato(4)})
+candidato1.addEventListener("click", function() { addVotosCandidato(1); });
+candidato2.addEventListener("click", function() { addVotosCandidato(2); });
+candidato3.addEventListener("click", function() { addVotosCandidato(3); });
+candidato4.addEventListener("click", function() { addVotosCandidato(4); });
 
-
-function addVotosCandidato(nroCandidato){
-        vetorVotos[nroCandidato - 1];
-        document.getElementById(`votosCand${nroCandidato}`).innerHTML = vetorVotos[nroCandidato - 1];
-    }
+function addVotosCandidato(nroCandidato) {
+    vetorVotos[nroCandidato - 1]++;
+    document.getElementById(`votosCand${nroCandidato}`).innerHTML = vetorVotos[nroCandidato - 1];
+}
